db59a826d98df2ab43c08344ed8167d6d2b92126
Ali Ghajarnia <alig@Alis-Mac-mini.local>
2014-11-15 01:13:25 -0600

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.h
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -2,13 +2,12 @@
 //  Gameplay.h
 //  PeevedPenguins
 //
-//  Created by Ali Ghajarnia on 11/14/14.
+//  Created by Ali Ghajarnia on 11/15/14.
 //  Copyright (c) 2014 Apportable. All rights reserved.
 //
 
 #import "CCNode.h"
 
 @interface Gameplay : CCNode
-
 
 @end
